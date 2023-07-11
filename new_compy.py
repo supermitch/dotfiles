a2c6e943a12094e1456c870841e8725e5d3b1020
Mitch LeBlanc <supermitch@gmail.com>
2023-07-11 16:40:12 -0600

    Update Python scripts to modernize

--- new_compy.py
+++ new_compy.py
@@ -1,23 +1,28 @@
 #!/usr/bin/env python
-""" This program installs everything you need to make a computer useful. """
-
+"""
+This program installs everything you need to make a computer useful.
+"""
 import os
 import subprocess
 
+
 def create_folders():
     """ Create common folders """
+    print('Creating folders')
     home = os.path.expanduser('~')
     folders = ['.vim']
     for folder in folders:
         dir_name = os.path.join(home, folder)  # New symlink
         if os.path.exists(dir_name):
-            print('\tDir exists - skipped {}'.format(dir_name))
+            print(f'\tDir exists - skipped {dir_name}')
         else:
             os.makedirs(dir_name)
-            print('\tMade dir {}'.format(dir_name))
+            print(f'\tMade dir {dir_name}')
+
 
 def git_clones():
     """ Clones git modules to install various stuff. """
+    print('Cloning git repos')
     git_repos = [  # (from url, to folder),
         ('gmarik/Vundle.vim.git', '~/.vim/bundle/Vundle.vim'),
     ]
@@ -25,40 +30,37 @@ def git_clones():
     for repo, to_folder in git_repos:
         if to_folder.startswith('~'):  # Expand home if required
             to_path = os.path.expanduser(to_folder)
-        print('\t' + repo)
-        subprocess.call(['git', 'clone','git@github.com:' + repo, to_path])
+        print(f'\t{repo}')
+        subprocess.call(['git', 'clone', f'git@github.com:{repo}', to_path])
+
 
 def brew():
     """ Brew install packages """
     pass
 
+
 def bew_cake():
     """ Brew cake install packages """
     pass
 
+
 def misc():
     """ Random odds & ends """
-
+    print('Miscellaneous')
     sources = ['~/.bashrc', '~/.bash_profile']
     for source in sources:
         source = os.path.expanduser(source)
         print(source)
         subprocess.call(['bin/sh/', source])  # Source RC files
-    subprocess.call(['vi', '-c',':PluginInstall'])  # Run Vundle
+    subprocess.call(['vi', '-c', ':PluginInstall'])  # Run Vundle
+
 
 def main():
     """ Make it so """
-
-    print('Creating folders')
     create_folders()
-
-    print('Cloning git repos')
     git_clones()
-
-    print('Miscellaneous')
     misc()
 
 
 if __name__ == '__main__':
     main()
-
--- symlinker.py
+++ symlinker.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
-
 import glob
 import os
 import shutil
 
+
 def backup_and_remove(full_path):
-    """ Copy a backup and remove original. """
+    """Copy a backup and remove original."""
     dir_name, file_name = os.path.split(full_path)
     backup_dir = os.path.join(dir_name, 'dotfiles_backup')
     if not os.path.exists(backup_dir):
         os.makedirs(backup_dir)
     backup_path = os.path.join(backup_dir, file_name)
     shutil.copy(full_path, backup_path)  # Copy orig to backup dir
-    print('\tBacked up to {}'.format(backup_path))
+    print(f'\tBacked up to {backup_path}')
     os.remove(full_path)  # Remove orig
 
+
 def create_symlink(source, home):
-    """ From a source path, create a symlink in home folder.  """
+    """From a source path, create a symlink in home folder."""
     file_name = os.path.split(source)[1]  # Filename only
     link_name = os.path.join(home, file_name)  # New symlink
     print(link_name)
@@ -26,7 +27,7 @@ def create_symlink(source, home):
             return None
         elif os.path.isfile(link_name):
             print('\tFile exists - backed up and removed')
-            backup_and_remove(link_name)  # Backup & remove file
+            backup_and_remove(link_name)
         else:  # A folder? Do nothing.
             print('\tPath exists - skipped')
             return None
@@ -35,21 +36,18 @@ def create_symlink(source, home):
     print('\tLink added')
     return link_name
 
-def main():
-    """ Fire ze missiles. """
 
-    home = os.path.expanduser("~")
+def main():
+    home = os.path.expanduser('~')
     dotfiles_folder = os.path.dirname(os.path.realpath(__file__))
 
-    ignore_list = ['git', 'gitignore', 'swp', 'DS_Store']
-
-    # Files starting with . and not in ignore list
-    rc_files = [f for f in glob.glob(dotfiles_folder + '/.*') \
-                if not any(f.endswith(x) for x in ignore_list)]
-
+    dotfiles = glob.glob(f'{dotfiles_folder}/.*')
+    ignore_list = ['.git', '.gitignore', '.swp', '.DS_Store']
+    rc_files = [x for x in dotfiles if x not in ignore_list]
     print('Creating dotfile symlinks')
-    symlinks = [create_symlink(x, home) for x in rc_files]
+    for rc_file in rc_files:
+        create_symlink(rc_file, home)
+
 
 if __name__ == '__main__':
     main()
-
